41c90d81c530e129ae2aea39327ac2262d763d60
Eugene Molodkin <burivuh@gmail.com>
2013-07-28 22:09:59 +0400

    Static maps API docs

--- js/example-controller.js
+++ js/example-controller.js
@@ -7,6 +7,11 @@ function ExampleController($scope, $timeout, $log) {
     $scope.lon = 82.927810142519;
     $scope.zoom = 15;
 
+    $scope.staticLat = 55.7368;
+    $scope.staticLon = 37.632618;
+    $scope.staticZoom = 14;
+    $scope.staticMarkers = [[37.64272,55.7368,99], [37.632618,55.7361]];
+
     $scope.zoomControls = true;
     $scope.fullscreenControls = true;
 
