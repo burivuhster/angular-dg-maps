853ef60e048e6d176b0132610d0a697fb1a562b4
Eugene Molodkin <burivuh@gmail.com>
2013-08-17 13:11:23 +0400

    Update docs on markers

--- js/example-controller.js
+++ js/example-controller.js
@@ -3,27 +3,38 @@
 }());
 
 function ExampleController($scope, $timeout, $log) {
-    $scope.lat = 55.028936234826;
-    $scope.lon = 82.927810142519;
+    $scope.lat = 55.028;
+    $scope.lon = 82.925;
     $scope.zoom = 15;
 
     $scope.staticLat = 55.7368;
     $scope.staticLon = 37.632618;
     $scope.staticZoom = 14;
-    $scope.staticMarkers = [[37.64272,55.7368,99], [37.632618,55.7361]];
 
     $scope.zoomControls = true;
     $scope.fullscreenControls = true;
 
     $scope.markers = [
         {
-            latitude: 55.028936234826,
-            longitude: 82.927810142519,
+            latitude: 55.023,
+            longitude: 82.926,
+            draggable: false,
+
+            hint: 'Click me!',
+
+            onClick: function(ind) {
+                alert('click');
+            }
+        },
+        {
+            latitude: 55.028,
+            longitude: 82.922,
             draggable: true,
 
-            dragStop: function(marker) {
-                var pos = marker.getPosition();
-                console.log('Marker drag stop: ' + pos.lon + ' ' + pos.lat);
+            hint: 'Drag me!',
+
+            onClick: function(ind) {
+                alert('other marker click');
             }
         }
     ];
